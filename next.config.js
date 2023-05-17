e02d146f18fd38bc3b7b0fba361037d958d4bb08
Yakiyo <yakiyo1206@gmail.com>
2023-05-17 16:08:53 +0000

    feat: redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: 'https://github.com/Yakiyo/imitari',
+          permanent: false,
+        },
+      ];
+    },
+  };
