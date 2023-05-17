9f136974a8f160674936628019509c0d495694a1
Yakiyo <yakiyo1206@gmail.com>
2023-05-17 17:23:33 +0000

    refactor: move images to a separate dir

--- pages/api/abandon.ts
+++ pages/api/abandon.ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(request: NextApiRequest, response: NextApiResponse) {
+	const text = request.query.text;
+	if (!text || typeof text !== 'string') {
+		return response.status(400).json({
+			message: 'Missing required parameter `text` in query',
+		});
+	}
 	response.status(200).json({
 		name: 'John doe',
 		age: 56,
