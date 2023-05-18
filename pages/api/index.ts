02e8b1e15d09660bf2d08e0198a3a62f7c1e11c3
Yakiyo <yakiyo1206@gmail.com>
2023-05-18 11:49:15 +0000

    feat: api endpoint

---
+++ pages/api/index.ts
@@ -0,0 +1,14 @@
+import { withSwagger } from 'next-swagger-doc';
+
+const swaggerHandler = withSwagger({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Imitari',
+      version: '0.1.0',
+    },
+  },
+  apiFolder: 'pages/api',
+});
+
+export default swaggerHandler();
