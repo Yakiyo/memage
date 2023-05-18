259299dbb860a1b570dc4d6c0658d83a6fb0beb4
Yakiyo <yakiyo1206@gmail.com>
2023-05-18 16:11:18 +0000

    feat: update swagger defs

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,12 +1,22 @@
 /**
  * Swagger config for next-swagger-doc
+ *
+ * Specs: [https://swagger.io/specification/](https://swagger.io/specification/)
  */
 export const swaggerDefinitions = {
 	definition: {
 		openapi: '3.0.0',
 		info: {
 			title: 'Imitari',
-			version: '0.1.0',
+			version: '1.0.0',
+			description: 'An image manipulation api',
+			license: {
+				name: 'MIT',
+			},
+		},
+		externalDocs: {
+			description: 'Documentation',
+			url: `https://${process.env.VERCEL_URL ?? 'localhost:3000'}/docs`,
 		},
 	},
 	apiFolder: 'pages/api',
