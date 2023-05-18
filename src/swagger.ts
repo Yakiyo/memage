7ae4f443a8ac65659e1d104f24741c584dbe4813
Yakiyo <yakiyo1206@gmail.com>
2023-05-18 14:06:16 +0000

    feat: export swagger config

---
+++ src/swagger.ts
@@ -0,0 +1,13 @@
+/**
+ * Swagger config for next-swagger-doc
+ */
+export const swaggerDefinitions = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Imitari',
+        version: '0.1.0',
+      },
+    },
+    apiFolder: 'pages/api',
+  }
