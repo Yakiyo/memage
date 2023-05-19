df268fcf0ca201831bde58f71e8bf9897ffcb721
Yakiyo <yakiyo1206@gmail.com>
2023-05-19 14:59:04 +0000

    refactor: oas defs

--- src/nrf.ts
+++ src/nrf.ts
@@ -8,4 +8,19 @@ export const { defineCatchAllHandler, defineEndpoints } = NextRestFramework({
 		title: 'Imitari',
 		description: 'An image manipulation api used to create niche meme-istic images',
 	},
+	openApiSpecOverrides: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Imitari',
+			version: '1.0.0',
+			description: 'Image manipulation api to generate meme-ish images',
+			license: {
+				name: 'MIT',
+			},
+		},
+		externalDocs: {
+			description: 'Documentation',
+			url: `https://${process.env.VERCEL_URL ?? 'localhost:3000'}/docs`,
+		},
+	},
 });
