da2bf8dd944e053e5b50489924980be7e7f462ed
Yakiyo <yakiyo1206@gmail.com>
2023-05-19 16:56:13 +0000

    feat: abandoned

--- pages/api/abandon.ts
+++ pages/api/abandon.ts
@@ -4,6 +4,7 @@ import { output } from '@/response';
 import * as y from 'yup';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
+import { render, wrap } from '@/text';
 
 export default defineEndpoints({
 	openApiSpecOverrides: {
@@ -19,12 +20,19 @@ export default defineEndpoints({
 		},
 		output,
 		async handler({ req, res }) {
-			// const { text } = req.query;
-			const img = await readFile(join(process.cwd(), './assets/abandon.bmp')).catch(console.error);
+			const { text } = req.query;
+			const img = await readFile(join(process.cwd(), './assets/images/abandon.bmp')).catch(
+				console.error,
+			);
 			if (!img) {
-				throw 'Missing image file';
+				return res.status(400).json({
+					message: 'Interal error. Try again later',
+				});
 			}
-			const canvas = await canvasFromImage(img);
+			const { canvas } = await canvasFromImage(img);
+			const ctx = canvas.getContext('2d');
+			render(ctx, wrap(ctx, text, 320), 25, 440, 4);
+
 			res.setHeader('Content-Type', 'image/png');
 			res.end(canvas.toBuffer('image/png'));
 		},
