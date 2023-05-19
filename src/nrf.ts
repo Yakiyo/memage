775a55bcd26d0ae9b8d037a0a38b86cd053dbf8a
Yakiyo <yakiyo1206@gmail.com>
2023-05-19 14:34:12 +0000

    fix: json and yaml endpoints renaming

--- src/nrf.ts
+++ src/nrf.ts
@@ -2,6 +2,10 @@ import { NextRestFramework } from 'next-rest-framework';
 
 export const { defineCatchAllHandler, defineEndpoints } = NextRestFramework({
 	apiRoutesPath: '/pages/api/',
-	// openApiJsonPath: '/api/endpoints.json',
-	// openApiYamlPath: '/api/endpoints.yaml'
+	openApiJsonPath: '/api/endpoints.json',
+	openApiYamlPath: '/api/endpoints.yaml',
+	swaggerUiConfig: {
+		title: 'Imitari',
+		description: 'An image manipulation api used to create niche meme-istic images',
+	},
 });
