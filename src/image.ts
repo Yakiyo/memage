0d1034a8bdb48247a9e37fa6a46da54be760f80b
Yakiyo <yakiyo1206@gmail.com>
2023-05-25 16:14:29 +0000

    feat: image utility func

--- src/image.ts
+++ src/image.ts
@@ -1,5 +1,6 @@
 import { type Canvas, Image, createCanvas, GlobalFonts } from '@napi-rs/canvas';
 import { join } from 'path';
+import { request } from 'undici';
 
 /**
  * Utility function to generate canvas from an asset image
@@ -16,4 +17,24 @@ export async function canvasFromImage(bgFile: Buffer): Promise<{
 	ctx.drawImage(bg, 0, 0);
 	ctx.font = '24px verdana';
 	return { canvas, image: bg };
+}
+
+/**
+ * Utility function to fetch an image from a url. Errors are returned as string.
+ * A successfull execution returns an Image instance.
+ */
+export async function fetch_image(url: string) {
+	const { value, error } = await request(url)
+		.then((v) => ({ value: v, error: null }))
+		.catch((e) => ({ value: null, error: e }));
+
+	if (error) {
+		return `${error}`;
+	}
+	if (!value || !value.headers['Content-Type']?.includes('image')) {
+		return 'Invalid url provided. Url must point to a valid image';
+	}
+	const img = new Image();
+	img.src = Buffer.from(await value.body.arrayBuffer());
+	return img;
 }
